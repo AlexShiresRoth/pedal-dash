1c9efe708529f38caef44739db6edb9086a113ae
Alex Roth <alexroth96@gmail.com>
2019-12-12 14:08:02 -0500

    start to design pedal items

--- src/components/listings/PedalItems.js
+++ src/components/listings/PedalItems.js
@@ -1,21 +1,27 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import pedalStyles from './pedalstyles/PedalItem.module.scss';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import pedalStyles from "./pedalstyles/PedalItem.module.scss";
 const PedalItem = ({ listings }) => {
-	const pedals = listings.map((pedal, i) => {
-		console.log(pedal.photos[0]._links);
-		return (
-			<div className={pedalStyles.pedal} key={i}>
-				<p>{pedal.model}</p>
-				<img src={pedal.photos[0]._links ? pedal.photos[0]._links.thumbnail.href : ''} alt="guitar pedal" />
-			</div>
-		);
-	});
-	return <>{pedals}</>;
+  const [max, setMax] = useState(5);
+  const splitArr = listings.slice(0, max);
+  const pedals = splitArr.map((pedal, i) => {
+    return (
+      <div className={pedalStyles.pedal} key={i}>
+        <p>{pedal.model}</p>
+        <img
+          src={
+            pedal.photos[0]._links ? pedal.photos[0]._links.thumbnail.href : ""
+          }
+          alt="guitar pedal"
+        />
+      </div>
+    );
+  });
+  return <>{pedals}</>;
 };
 
 PedalItem.propTypes = {
-	listings: PropTypes.array.isRequired,
+  listings: PropTypes.array.isRequired
 };
 
 export default PedalItem;
